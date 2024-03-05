5163fd53f337c23307302a08abd96b4b50f1659e
Joshua Hale <joshuahale829@gmail.com>
2024-03-05 10:08:06 -0600

    wrote models -> post.js

---
+++ models/post.js
@@ -0,0 +1,46 @@
+// Create the Post model for the database.
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create the Post model
+class Post extends Model {}
+
+// Define the Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+        timestamps: true
+    }
+);
+
+// Export the Post model
+module.exports = Post;
