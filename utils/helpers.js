469099743cd57abf5253ac824f9ce66f41843499
Joshua Hale <joshuahale829@gmail.com>
2024-03-08 15:00:43 -0600

    Fixed syntax error in schema.sql

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,13 @@
 // Helper function for formatting date
-module.exports = { 
+module.exports = {
     format_date: (date) => {
-        const date = new Date(date);
-        const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-        const hours = date.getHours().toString().padStart(2, '0'); 
-        const minutes = date.getMinutes().toString().padStart(2, '0');
-        const formattedTime = `${hours}:${minutes}`;
-        return `${formattedDate} at ${formattedTime}`;
-    }
-};
+      // Format date and time information to MM/DD/YYYY HH:mm
+      const d = new Date(date);
+      const formattedDate = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
+      const hours = d.getHours().toString().padStart(2, '0');
+      const minutes = d.getMinutes().toString().padStart(2, '0');
+      const formattedTime = `${hours}:${minutes}`;
+      
+      return `${formattedDate} at ${formattedTime}`;
+    },
+  };
