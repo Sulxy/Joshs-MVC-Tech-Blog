7eb27700cee3ca3b084d6455a00f47980788b7c7
Joshua Hale <joshuahale829@gmail.com>
2024-03-08 09:42:26 -0600

    Wrote public -> js -> logout.js

---
+++ public/js/logout.js
@@ -0,0 +1,19 @@
+// logout function to sign out the user
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log out');
+    }
+};
+
+// Event listener for logout button
+const logoutBtn = document.querySelector('#logout-btn');
+if (logoutBtn) {
+    logoutBtn.addEventListener('click', logout);
+}
