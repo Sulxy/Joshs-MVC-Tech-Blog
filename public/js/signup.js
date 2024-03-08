8776d15ceb39a4cf9f2590376ade315b3a2d5533
Joshua Hale <joshuahale829@gmail.com>
2024-03-08 09:49:22 -0600

    Wrote public -> js -> signup.js

---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+const signUp = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+
+    if (username && password && email) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, password, email }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to sign up');
+        }
+    }
+};
+
+// Event listener for sign up button
+const signUpBtn = document.querySelector('#signup-btn');
+if (signUpBtn) {
+    signUpBtn.addEventListener('click', signUp);
+}
