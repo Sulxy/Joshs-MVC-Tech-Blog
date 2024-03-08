3a8f57ef8e044a779131b05d8128ac08e99555da
Joshua Hale <joshuahale829@gmail.com>
2024-03-08 13:47:51 -0600

    changed 'd' variable to 'date' to make it easier to read in helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,10 @@
 // Helper function for formatting date
 module.exports = { 
     format_date: (date) => {
-        const d = new Date(date);
-        const formattedDate = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
-        const hours = d.getHours().toString().padStart(2, '0'); 
-        const minutes = d.getMinutes().toString().padStart(2, '0');
+        const date = new Date(date);
+        const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        const hours = date.getHours().toString().padStart(2, '0'); 
+        const minutes = date.getMinutes().toString().padStart(2, '0');
         const formattedTime = `${hours}:${minutes}`;
         return `${formattedDate} at ${formattedTime}`;
     }
