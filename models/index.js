c8664245bd66138a323544e9d5b3d4dd80751bc6
Joshua Hale <joshuahale829@gmail.com>
2024-03-10 15:35:07 -0500

    Removed redundant associations in models\index.js, and corrected postData array from post_text: to content:

--- models/index.js
+++ models/index.js
@@ -4,35 +4,35 @@ const Post = require('./Post');
 const Comment = require('./comment');
 
 // Create Associations
-User.hasMany(Post, {
-  foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-});
+// User.hasMany(Post, {
+//   foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-});
+// User.hasMany(Comment, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+    onDelete: 'CASCADE'
     });
 
 Post.hasMany(Comment, {
     foreignKey: 'post_id',
-    onDelete: 'SET NULL'
+    onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+    onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-    onDelete: 'SET NULL'
-});
+// Comment.belongsTo(Post, {
+//     foreignKey: 'post_id',
+//     onDelete: 'CASCADE'
+// });
 
 // Export Models
 module.exports = { User, Post, Comment };
