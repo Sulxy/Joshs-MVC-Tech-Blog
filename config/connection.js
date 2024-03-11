b024a5f6f9e43eedcda9a9be496e52a43145b301
Joshua Hale <joshuahale829@gmail.com>
2024-03-11 15:28:34 -0500

    Commented out connection.js stuff.

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,23 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // Create a new Sequelize object and connect to the database
-const sequelize = process.env.JAWSDB_URL // <-- JAWSDB_URL is an environment variable that Heroku provides
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, { // localhost
-            host: "localhost",
-            dialect: "mysql",
-            dialectOptions: {
-                decimalNumbers: true,
-            },
-        });
+// const sequelize = process.env.JAWSDB_URL // <-- JAWSDB_URL is an environment variable that Heroku provides
+//     ? new Sequelize(process.env.JAWSDB_URL)
+//     : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, { // localhost
+//             host: "localhost",
+//             dialect: "mysql",
+//             dialectOptions: {
+//                 decimalNumbers: true,
+//             },
+//         });
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: "localhost",
+    dialect: "mysql",
+    dialectOptions: {
+        decimalNumbers: true,
+    },
+});
 
 // Export the sequelize object
 module.exports = sequelize;
