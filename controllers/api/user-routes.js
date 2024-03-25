dab194f3a255a93a978d5955e448887cce2a7624
Joshua <Joshuahale829@gmail.com>
2024-03-25 14:32:43 -0500

    Typoes for logged_In functionality.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,7 +55,7 @@ router.post('/login', async (req, res) => {
 
         req.session.save(() => {
             req.session.user_id = userData.id;
-            req.session.logged_in = true;
+            req.session.logged_In = true;
 
             res.status(200).json({ user: userData, message: 'You are now logged in!' });
         });
